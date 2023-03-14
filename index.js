41ea5a3ac1a5ec4fbf04beb229e34f99b9252cf4
James Akinsanya <j.akins24@gmail.com>
2023-03-13 21:57:27 -0400

    commit tree 00381c140fcb63b72ac6c1017958023d596424f4

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16
+const num4 = 12
+
+let mod = num3 % num4
+
+const max = Math.max(1, 20, 9, 18, 11, 15, 2, 5, 10)
